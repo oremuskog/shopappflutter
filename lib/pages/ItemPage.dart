4fa4428b90c6c0aaf3ebfb73f4b9a99eb4964fc8
Ørémus <salesodjo@gmail.com>
2023-09-26 09:02:41 -0700

    commut updated

--- lib/pages/ItemPage.dart
+++ lib/pages/ItemPage.dart
@@ -1,4 +1,5 @@
 import 'package:clippy_flutter/clippy_flutter.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:shop/widgets/ItemAppBar.dart';
@@ -56,7 +57,36 @@ class ItemPage extends StatelessWidget{
                             direction: Axis.horizontal,
                             itemCount: 5,
                             itemSize: 20,
-                            
+                            itemPadding: EdgeInsets.symmetric(horizontal: 2),
+                            itemBuilder: (context, _) => Icon(
+                              Icons.favorite,
+                              color: Color(0xFF4C53A5)
+                            ),
+                            onRatingUpdate: (index){},
+                          ),
+
+                          Row(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 3,
+                                      blurRadius: 10,
+                                      offset: Offset(0, 3)
+                                    ),
+                                  ],
+                                ),
+                                child: Icon(
+                                  CupertinoIcons.minus,
+                                  size: 18,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
